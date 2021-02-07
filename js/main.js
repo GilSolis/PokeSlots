ee8a5529a08c56abe8277a536d64c4b69964a5f9
Gil Solis <gilsolisuw@gmail.com>
2021-02-07 14:54:41 -0800

    3 reels working

--- js/main.js
+++ js/main.js
@@ -1,64 +1,67 @@
 document.querySelector('button').addEventListener('click', spin)
 
 function spin() {
-    let randomNumber = Math.floor(Math.random() * 10 + 20)
-    let randomNumber2 = Math.floor(Math.random() * 10 + 25)
+    let slot1RandomNumber = Math.floor(Math.random() * 10 + 20)
+    let slot2RandomNumber = Math.floor(Math.random() * 10 + 35)
+    let slot3RandomNumber = Math.floor(Math.random() * 10 + 50)
     let slot1 = document.getElementById('img1')
     let slot2 = document.getElementById('img2')
-    let arr = ['img/1.svg', 'img/2.svg', 'img/3.svg', 'img/4.svg', 'img/5.svg',]
-    let testingNumber = 0
+    let slot3 = document.getElementById('img3')
+    let arr = ['img/1.svg', 'img/2.svg', 'img/3.svg', 'img/pokeball.svg', 'img/5.svg',]
+    let testingNumber1 = 0
+    let testingNumber2 = 0
+    let testingNumber3 = 0
     let i = 0
+    let j = 0
+    let k = 0
+
     spin2win = setInterval(spin1, 50)
     spin2win2 = setInterval(spin2, 50)
+    spin2win3 = setInterval(spin3, 50)
 
-    console.log(randomNumber)
+    console.log(slot1RandomNumber, slot2RandomNumber, slot3RandomNumber)
 
 
     function spin1() {
-        testingNumber++
+
+        testingNumber1++
         i++
-        if (testingNumber >= randomNumber) {
+        if (testingNumber1 >= slot1RandomNumber) {
             clearInterval(spin2win)
         }
-        console.log(i)
         if (i === 5) {
             i = 0
             slot1.src = arr[i]
         } else {
             slot1.src = arr[i]
         }
-
     }
     function spin2() {
-        testingNumber++
-        i++
-        if (testingNumber >= randomNumber2) {
+        testingNumber2++
+        j++
+        if (testingNumber2 >= slot2RandomNumber) {
             clearInterval(spin2win2)
         }
-        console.log(i)
-        if (i === 5) {
-            i = 0
-            slot2.src = arr[i]
+        if (j === 5) {
+            j = 0
+            slot2.src = arr[j]
         } else {
-            slot2.src = arr[i]
+            slot2.src = arr[j]
+        }
+    }
+    function spin3() {
+        testingNumber3++
+        k++
+        if (testingNumber3 >= slot3RandomNumber) {
+            clearInterval(spin2win3)
         }
 
+        if (k === 5) {
+            k = 0
+            slot3.src = arr[k]
+        } else {
+            slot3.src = arr[k]
+        }
     }
-
-
-    // let random = Math.floor(Math.random() * 5)
-    // let random1 = Math.floor(Math.random() * 5)
-    // let random2 = Math.floor(Math.random() * 5)
-    // document.getElementById('img1').src = arr[random]
-    // document.getElementById('img2').src = arr[random1]
-    // document.getElementById('img3').src = arr[random2]
-
-    // setInterval(function () {
-
-    //     for (let i = 0; i <= 5; i++) {
-    //         document.querySelector('img').src = "img/" + i + ".svg"
-    //     }
-
-    // }, 1000);
 
 }
